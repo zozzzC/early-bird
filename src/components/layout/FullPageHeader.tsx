732d8f4eb96e3a89d69b67c406e0ecd3d8517ebc
zozzzC <czlc05edu@gmail.com>
2025-03-10 16:45:20 +1300

    create compose.yaml

--- src/components/layout/FullPageHeader.tsx
+++ src/components/layout/FullPageHeader.tsx
@@ -3,7 +3,7 @@ import logo from "@/test/logoipsum-361.svg";
 
 export default function FullPageHeader() {
   return (
-    <div className="w-full bg-neutral-500 h-20 opacity-25 flex justify-between items-center">
+    <div className="z-10 w-full bg-neutral-500 h-20 opacity-25 flex justify-between items-center">
       <Image src={logo} alt="logo" className="m-5" />
       <div className="flex gap-5 m-5">
         <p>home</p>
---
+++ src/components/layout/HomepageCarousel.tsx
@@ -0,0 +1,10 @@
+import { Carousel } from "@mantine/carousel"
+export default function HomepageCarousel() {
+    return (
+        <div>
+            <Carousel>
+                
+            </Carousel>
+        </div>
+    )
+}

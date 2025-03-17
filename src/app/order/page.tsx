57df964cd181817711f981a532ba782b7d12d9a2
zozzzC <czlc05edu@gmail.com>
2025-03-17 16:18:41 +1300

    fixed image clipping in order

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -8,15 +8,17 @@ export default function Order() {
   return (
     <div>
       <p>order</p>
-      <div className="flex md:flex-row flex-col gap-5 md:justify-center items-center flex-wrap">
-        {orderData.map((i) => (
-          <OrderItem
-            key={i.key}
-            name={i.name}
-            description={i.description}
-            photo={i.photo}
-          />
-        ))}
+      <div className="flex justify-center m-0">
+        <div className="w-9/12 grid 2xl:grid-cols-4 lg:grid-cols-3 grid-cols-1 m-0 gap-5">
+          {orderData.map((i) => (
+            <OrderItem
+              key={i.key}
+              name={i.name}
+              description={i.description}
+              photo={i.photo}
+            />
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/components/order/OrderItem.tsx
+++ src/components/order/OrderItem.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 
 export default function OrderItem({ name, description, photo }: Item) {
   return (
-    <div className="h-80 w-80 flex flex-col">
-      <div className="h-80 w-80 relative">
-        <Image src={photo.toString()} alt={name} fill />
+    <div className="h-full flex flex-col relative items-center">
+      <div className="grow-1 object-cover w-full h-full max-w-80 relative aspect-square">
+        <Image src={photo.toString()} alt={name} objectFit="cover" fill />
       </div>
       <p>{name}</p>
       <p>{description}</p>

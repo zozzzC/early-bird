f4456527f49d9df11c357123531098455b2c6fc1
zozzzC <czlc05edu@gmail.com>
2025-03-22 14:54:07 +1300

    created total text and number input for modal

--- src/components/order/OrderItemModal.tsx
+++ src/components/order/OrderItemModal.tsx
@@ -2,8 +2,12 @@ import Image from "next/image";
 import SingleSelectButton from "./options/SingleSelectButton";
 import SingleSelectManager from "./options/SingleSelectManager";
 import MultiSelectManager from "./options/MultiSelectManager";
+import { NumberInput } from "@mantine/core";
+import { useState } from "react";
 
 export default function OrderItemModal({ id }: { id: number }) {
+  const [quantity, setQuantity] = useState<number>(1);
+
   return (
     <div className="w-full h-full grid lg:grid-cols-2 grid-cols-1">
       <div className="w-full lg:h-full lg:max-h-full aspect-square relative">
@@ -16,19 +20,37 @@ export default function OrderItemModal({ id }: { id: number }) {
         />
       </div>
       <div className="flex-1">
-        <p>Order Item {id}</p>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
-          consectetur sed orci sed sagittis. Aenean accumsan luctus justo, non
-          mattis enim ornare id. Aliquam justo nibh, sodales ut dui vel, varius
-          mattis ipsum. Morbi venenatis, odio sed rutrum tincidunt, arcu felis
-          sodales velit, a porta lorem felis eu ante. In hac habitasse platea
-          dictumst. Sed sodales sem a leo feugiat, in elementum sem accumsan.
-        </p>
-        <p>Options</p>
-        <SingleSelectManager />
-        <p>Add Ons</p>
-        <MultiSelectManager />
+        <div className="flex flex-col h-full">
+          <div>
+            <p>Order Item {id}</p>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec
+              consectetur sed orci sed sagittis. Aenean accumsan luctus justo,
+              non mattis enim ornare id. Aliquam justo nibh, sodales ut dui vel,
+              varius mattis ipsum. Morbi venenatis, odio sed rutrum tincidunt,
+              arcu felis sodales velit, a porta lorem felis eu ante. In hac
+              habitasse platea dictumst. Sed sodales sem a leo feugiat, in
+              elementum sem accumsan.
+            </p>
+            <p>Options</p>
+            <SingleSelectManager />
+            <p>Add Ons</p>
+            <MultiSelectManager />
+          </div>
+          <div className="p-5 gap-5 flex justify-end w-full items-center">
+            <NumberInput
+              size="md"
+              className="w-1/6 "
+              variant="filled"
+              min={1}
+            ></NumberInput>
+          </div>
+          <div className="flex justify-end w-full p-5 ">
+            <div className="">
+              <p className="text-lg">total</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );

65165f73dd14e8bdeaf3dc270c1f80ec4b35f217
Fluorineeee <shiyanwang1412@gmail.com>
2025-03-26 11:36:10 +1300

    fixed image size on header, still need style header more

--- src/components/layout/FullPageHeader.tsx
+++ src/components/layout/FullPageHeader.tsx
@@ -4,9 +4,12 @@ import Link from "next/link";
 
 export default function FullPageHeader() {
   return (
-    <div className="w-full h-20 flex justify-between items-center">
-      <Image src={logo} alt="logo" className="m-5" />
-      <div className="flex gap-5 m-5">
+
+    <div className="w-full bg-orange-main h-30 flex justify-between">
+        <div className="h-full w-36 flex justify-center">
+          <Image src={logo} alt="logo" className="flex-1" />
+        </div>
+      <div className="flex gap-5 m-10">
         <Link href="/">home</Link>
         <Link href="/about">about</Link>
         <Link href="/order">order</Link>

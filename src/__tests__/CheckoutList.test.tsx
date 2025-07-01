5e13f73bd21ab1ce6858c23a1573eafdf076b066
zozzzC <czlc05edu@gmail.com>
2025-07-01 17:47:40 +1200

    add test for clicking pay button if item in cart is out of stock

--- src/__tests__/CheckoutList.test.tsx
+++ src/__tests__/CheckoutList.test.tsx
@@ -4,6 +4,7 @@ import sampleOrderItemsAmericanoOutOfStock from "@/__tests__/sample/sampleOrderI
 import CheckoutList from "@/components/checkout/CheckoutList";
 import CartProviderComponent from "@/components/order/CartProviderComponent";
 import { render, screen } from "@/helpers/test-utils";
+import userEvent from "@testing-library/user-event";
 
 describe("out of stock tests", () => {
   it("if an item in the cart is out of stock, the out of stock text appears", async () => {
@@ -21,9 +22,25 @@ describe("out of stock tests", () => {
     );
   });
 
-  it.todo(
-    "if an item in cart is out of stock, clicking the pay now button pulls up the warning modal"
-  );
+  it("if an item in cart is out of stock, clicking the pay now button pulls up the warning modal", async () => {
+    render(
+      <CartProviderComponent
+        defaultItems={defaultItems}
+        defaultItemsArray={defaultItemsArray}
+      >
+        <CheckoutList orderItems={sampleOrderItemsAmericanoOutOfStock} />
+      </CartProviderComponent>
+    );
+
+    const payNow = screen.getByRole("button", { name: "pay now" });
+    await userEvent.click(payNow);
+
+    expect(
+      screen.findByText(
+        "you have unavailable items in your cart. please remove them from your cart to continue."
+      )
+    ).toBeDefined;
+  });
 
   it.todo(
     "when only 1 of 2 out of stock items are removed from the cart, the pay now button still pulls up the warning modal"
--- src/components/checkout/InvalidOrderModal.tsx
+++ src/components/checkout/InvalidOrderModal.tsx
@@ -4,7 +4,7 @@ export default function InvalidOrderModal({close} : {close(): void}) {
   return (
     <div className="flex w-full flex-col items-center justify-center">
       <p>
-        You have unavailable items in your cart. Please remove them from your
+        you have unavailable items in your cart. please remove them from your
         cart to continue.
       </p>
       <Button onClick={close}>got it</Button>

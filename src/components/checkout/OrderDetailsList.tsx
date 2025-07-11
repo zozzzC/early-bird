93076491eaf6cc59a75dd8bdf5ee28ec382a75df
zozzzC <czlc05edu@gmail.com>
2025-07-11 16:37:30 +1200

    update mantine to 8.0.0, added DateTimePicker to OrderDetailsList

--- src/components/checkout/OrderDetailsList.tsx
+++ src/components/checkout/OrderDetailsList.tsx
@@ -1,8 +1,8 @@
-"use client"
-import "@mantine/dates/styles.css";
-import { Input, TextInput } from "@mantine/core";
-import { useForm } from "@mantine/form";
+"use client";
+import { TextInput } from "@mantine/core";
 import { DateTimePicker } from "@mantine/dates";
+import "@mantine/dates/styles.css";
+import { useForm } from "@mantine/form";
 
 export default function OrderDetailsList() {
   const form = useForm({
@@ -18,20 +18,30 @@ export default function OrderDetailsList() {
   const minDate = new Date();
   minDate.setHours(minDate.getHours() + 2);
   return (
-    <div className="p-5">
+    <div className="p-5 flex flex-col items-center ">
       <TextInput
+        className="w-2/3"
         withAsterisk
         label="name"
         key={form.key("name")}
         {...form.getInputProps("name")}
       />
       <TextInput
+        className="w-2/3"
         withAsterisk
         label="email"
         key={form.key("email")}
         {...form.getInputProps("email")}
       />
-      <DateTimePicker label="pickup date and time" minDate={minDate} />
+      <DateTimePicker
+        label="pickup date and time"
+        timePickerProps={{
+          withDropdown: true,
+          popoverProps: { withinPortal: false },
+          minutesStep: 10,
+          format: "12h",
+        }}
+      />
     </div>
   );
 }

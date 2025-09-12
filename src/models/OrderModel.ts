5ff06997e62581471c68257d76d6b032baaab624
zozzzC <czlc05edu@gmail.com>
2025-09-12 22:31:43 +1200

    create mongodb model for order

---
+++ src/models/OrderModel.ts
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from "mongoose";
+const orderItemAddOnSchema = new Schema({
+  name: { type: String, required: true },
+  price: { type: String, required: true },
+});
+
+const orderItemSchema = new Schema({
+  name: { type: String, required: true },
+  category: String,
+  basePrice: { type: String, required: true },
+  price: { type: Number, required: true },
+  quantity: { type: Number, required: true },
+  size: orderItemAddOnSchema,
+  milk: orderItemAddOnSchema,
+  extra: [orderItemAddOnSchema],
+});
+
+const orderSchema = new Schema({
+  name: String,
+  email: String,
+  phone: String,
+  pickupDate: Date,
+  createdDate: Date,
+  itemsArray: [orderItemSchema],
+});
+
+export const OrderModel =
+  mongoose.models.Order || mongoose.model("Order", orderSchema);

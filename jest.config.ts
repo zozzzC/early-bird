e94dc65161b99f177e02a48f6f05f71e242a7891
zozzzC <czlc05edu@gmail.com>
2025-09-12 21:27:54 +1200

    change jest environment to use node otherwise mongoose will not work

--- jest.config.ts
+++ jest.config.ts
@@ -9,7 +9,8 @@ const createJestConfig = nextJest({
 // Add any custom config to be passed to Jest
 const config: Config = {
   coverageProvider: "v8",
-  testEnvironment: "jsdom",
+  testEnvironment: "node",
+  transformIgnorePatterns: ["node_modules/(?!@ngrx|(?!deck.gl)|ng-dynamic)"],
   // Add more setup options before each test is run
   // setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
 };
